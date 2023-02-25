314066e680368ea3aae23c76699796c3c4093fed
Mansur Besleney <besleneymansur@gmail.com>
2023-02-25 15:40:25 +0300

    Player Rotation & camera control added

--- Assets/Scripsts/PlayerController.cs
+++ Assets/Scripsts/PlayerController.cs
@@ -8,14 +8,25 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float walkSpeed = 8f;
     [SerializeField] private float runSpeed = 12f;
 
+    [Header("Mouse Control Settings")]
+    [SerializeField] private float mouseSensivity = 1f;
+    [SerializeField] private float maxViewAngle = 60f;
+
     private CharacterController characterController;
     private float currentSpeed = 8f;
     private float horizontalInput;
     private float verticalInput;
+    private Transform mainCamera;
     
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
+
+        if(Camera.main.GetComponent<CameraController>() == null)
+        {
+            Camera.main.gameObject.AddComponent<CameraController>();
+        }
+        mainCamera = GameObject.FindWithTag("CameraPoint").transform;
     }
     void Update()
     {
@@ -23,7 +34,34 @@ public class PlayerController : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {   
+    {
+        Move();
+
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+        //oyuncunun sağa sola rotasyonunu ayarlıyoruz
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x , transform.eulerAngles.y + MouseInput().x , transform.eulerAngles.z);
+        
+        if(mainCamera != null)//oyuncunun dikeyde baktığı yönü ayarlıyoruz
+        {
+            mainCamera.rotation = Quaternion.Euler(mainCamera.rotation.eulerAngles + new Vector3(-MouseInput().y , 0f, 0f));
+
+            if(mainCamera.eulerAngles.x > maxViewAngle && mainCamera.eulerAngles.x <180f)
+            {
+                mainCamera.rotation = Quaternion.Euler(maxViewAngle, transform.eulerAngles.y, transform.eulerAngles.z);
+            }
+            else if(mainCamera.eulerAngles.x > 180f && mainCamera.eulerAngles.x < 360f - maxViewAngle)
+            {
+                mainCamera.rotation = Quaternion.Euler(360f- maxViewAngle , transform.eulerAngles.y, transform.eulerAngles.z);
+            }
+        }
+    }
+
+    private void Move()
+    {
         Vector3 localVerticalVector = transform.forward * verticalInput;
         Vector3 localHorizontalVector = transform.right * horizontalInput;
         Vector3 movementVector = localHorizontalVector + localVerticalVector;
@@ -46,5 +84,10 @@ public class PlayerController : MonoBehaviour
         {
             currentSpeed = walkSpeed;
         }
+    }
+
+    private Vector2 MouseInput()
+    {
+        return new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensivity;
     }
 }
